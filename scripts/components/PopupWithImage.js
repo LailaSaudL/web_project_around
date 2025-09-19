599c702050ed4fc38c6ebe98406ff1a6c2b4bec3
LailaSaudL <lailasaud98@gmail.com>
2025-09-19 14:25:28 +0200

    Update PopupWithImage.js

--- scripts/components/PopupWithImage.js
+++ scripts/components/PopupWithImage.js
@@ -1,1 +1,18 @@
+import { Popup } from './Popup.js';
 
+export class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._image = this.popup.querySelector('.popup__img');
+    this._caption = this.popup.querySelector('.popup__img-title');
+  }
+
+  open(name, link) {
+    if (this._image) {
+      this._image.src = link;
+      this._image.alt = name;
+    }
+    if (this._caption) this._caption.textContent = name;
+    super.open();
+  }
+}
