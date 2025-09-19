51de5a144525eb0467f1be9d219dc1c7c8bfdfd5
LailaSaudL <lailasaud98@gmail.com>
2025-09-19 14:30:52 +0200

    Create UserInfo.js

---
+++ scripts/components/UserInfo.js
@@ -0,0 +1,18 @@
+export class UserInfo {
+  constructor({ nameSelector, jobSelector }) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._jobElement = document.querySelector(jobSelector);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._nameElement ? this._nameElement.textContent : '',
+      job: this._jobElement ? this._jobElement.textContent : ''
+    };
+  }
+
+  setUserInfo({ name, job }) {
+    if (this._nameElement) this._nameElement.textContent = name;
+    if (this._jobElement) this._jobElement.textContent = job;
+  }
+}
