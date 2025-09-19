9d7a61f1192bdcee2cd75893fa8e03424190c6af
LailaSaudL <lailasaud98@gmail.com>
2025-09-19 14:27:07 +0200

    Update Section.js

--- scripts/components/Section.js
+++ scripts/components/Section.js
@@ -1,1 +1,21 @@
+export class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._renderedItems = items || [];
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
 
+  renderItems() {
+    // opcionalmente limpiar
+    // this._container.innerHTML = '';
+    this._renderedItems.forEach(item => this._renderer(item));
+  }
+
+  addItem(element, toStart = true) {
+    if (toStart) {
+      this._container.prepend(element);
+    } else {
+      this._container.append(element);
+    }
+  }
+}
