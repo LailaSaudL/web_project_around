dfa07af3d5fda9550ae1be7344726fa4da9d3d97
LailaSaudL <lailasaud98@gmail.com>
2025-09-19 14:39:56 +0200

    Update Card.js

--- scripts/Card.js
+++ scripts/Card.js
@@ -7,36 +7,39 @@ export class Card {
     this._handleCardClick = handleCardClick;
   }
 
-  _getTemplate() {
-    const cardElement = document
+_getTemplate() {
+    return document
       .querySelector(this._templateSelector)
       .content.querySelector(".card")
       .cloneNode(true);
-    return cardElement;
   }
 
   _setEventListeners() {
-    this._element.querySelector(".card__like-button").addEventListener("click", () => {
-      this._handleLike();
+    this._likeButton.addEventListener("click", () => {
+      this._toggleLike();
     });
-    this._element.querySelector(".card__delete-button").addEventListener("click", () => {
+    this._deleteButton.addEventListener("click", () => {
       this._element.remove();
     });
-    this._element.querySelector(".card__image").addEventListener("click", () => {
+    this._image.addEventListener("click", () => {
       this._handleCardClick(this._name, this._link);
     });
   }
 
-  _handleLike() {
-    this._element.querySelector(".card__like-button").classList.toggle("card__like-button_active");
+  _toggleLike() {
+    this._likeButton.classList.toggle("card__like-button_active");
   }
 
   generateCard() {
     this._element = this._getTemplate();
-    this._element.querySelector(".card__title").textContent = this._name;
-    const cardImage = this._element.querySelector(".card__image");
-    cardImage.src = this._link;
-    cardImage.alt = this._name;
+    this._title = this._element.querySelector(".card__title");
+    this._image = this._element.querySelector(".card__image");
+    this._likeButton = this._element.querySelector(".card__like-button");
+    this._deleteButton = this._element.querySelector(".card__delete-button");
+
+    this._title.textContent = this._name;
+    this._image.src = this._link;
+    this._image.alt = this._name;
 
     this._setEventListeners();
     return this._element;
