7a0847cdc34293b83d89fa47a04fd5f1d80b4eb6
LailaSaudL <lailasaud98@gmail.com>
2025-10-05 14:42:48 +0200

    Update PopupWithConfirmation.js

--- scripts/components/PopupWithConfirmation.js
+++ scripts/components/PopupWithConfirmation.js
@@ -1,1 +1,27 @@
+import { Popup } from "./Popup.js";
 
+export class PopupWithConfirmation extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._form = this._popup.querySelector("form");
+    this._submitHandler = null;
+  }
+
+  setSubmitAction(action) {
+    // action: () => Promise
+    this._submitHandler = action;
+  }
+
+  _handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (!this._submitHandler) return;
+    this._submitHandler()
+      .then(() => this.close())
+      .catch(err => console.error(err));
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", this._handleSubmit);
+  }
+}
