8fdbd80621cb58ca7ae51acf80fc6e97de5de3c9
LailaSaudL <lailasaud98@gmail.com>
2025-10-05 20:38:47 +0200

    Update UserInfo.js

--- scripts/components/UserInfo.js
+++ scripts/components/UserInfo.js
@@ -1,18 +1,24 @@
-export class UserInfo {
-  constructor({ nameSelector, jobSelector }) {
-    this._nameElement = document.querySelector(nameSelector);
-    this._jobElement = document.querySelector(jobSelector);
-  }
+export default class UserInfo {
+    constructor({ nameSelector, aboutSelector }) {
+        this._nameElement = document.querySelector(nameSelector);
+        this._aboutElement = document.querySelector(aboutSelector);
 
-  getUserInfo() {
-    return {
-      name: this._nameElement ? this._nameElement.textContent : '',
-      job: this._jobElement ? this._jobElement.textContent : ''
+    }
+
+    getUserInfo() {
+        return {
+        name: this._nameElement.textContent,
+        about: this._aboutElement.textContent
+
     };
-  }
+}
 
-  setUserInfo({ name, job }) {
-    if (this._nameElement) this._nameElement.textContent = name;
-    if (this._jobElement) this._jobElement.textContent = job;
-  }
+setUserInfo({ name, about }) {
+    this._nameElement.textContent = name;
+    this._aboutElement.textContent = about;
+}
+
+setUserAvatar(avatar) {
+    this._avatarElement.src = avatar;
+}
 }
