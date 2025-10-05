0cc1ecc5cf3cb25458ca5032c0d2f2545a0c0c0d
LailaSaudL <lailasaud98@gmail.com>
2025-10-05 16:04:47 +0200

    Update PopupWithConfirmation.js

--- scripts/components/PopupWithConfirmation.js
+++ scripts/components/PopupWithConfirmation.js
@@ -3,22 +3,28 @@ import { Popup } from "./Popup.js";
 export class PopupWithConfirmation extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._form = this._popup.querySelector(".popup__form");
+    this._form = this._popup ? this._popup.querySelector("form") : null;
+    this._submitHandler = null;
+    this._handleSubmit = this._handleSubmit.bind(this);
   }
 
   setSubmitAction(action) {
+    // action: () => Promise
     this._submitHandler = action;
+  }
+
+  _handleSubmit(evt) {
+    evt.preventDefault();
+    if (!this._submitHandler) return;
+    this._submitHandler()
+      .then(() => this.close())
+      .catch(err => console.error(err));
   }
 
   setEventListeners() {
     super.setEventListeners();
-    this._form.addEventListener("submit", (evt) => {
-      evt.preventDefault();
-      if (this._submitHandler) {
-        this._submitHandler()
-          .then(() => this.close())
-          .catch(err => console.error(err));
-      }
-    });
+    if (this._form) {
+      this._form.addEventListener("submit", this._handleSubmit);
+    }
   }
 }
