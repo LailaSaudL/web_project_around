20903c973aa39f39fbc0703e34caee32d2f40835
LailaSaudL <lailasaud98@gmail.com>
2025-10-12 11:32:56 +0200

    Update PopupWithConfirmation.js

--- scripts/components/PopupWithConfirmation.js
+++ scripts/components/PopupWithConfirmation.js
@@ -1,27 +1,57 @@
 import { Popup } from "./Popup.js";
 
+/**
+ * Popup con confirmación (botón Sí / submit)
+ * Se utiliza así:
+ *  const confirmPopup = new PopupWithConfirmation("#confirm-popup");
+ *  confirmPopup.setSubmitAction(() => api.deleteCard(id)); // debe devolver Promise
+ *  confirmPopup.setEventListeners();
+ *  confirmPopup.open();
+ */
 export class PopupWithConfirmation extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._form = this._popup ? this._popup.querySelector("form") : null;
+    this._form = this._popup ? this._popup.querySelector(".popup__form") : null;
+    // función a ejecutar al confirmar (debe devolver Promise)
     this._submitHandler = null;
-    this._handleSubmit = this._handleSubmit.bind(this);
+    this._onSubmit = this._onSubmit.bind(this);
   }
 
-setSubmitAction(action) {
-  this._handleSubmit = action;
-}
+  // set la acción que se ejecutará cuando el usuario confirme
+  setSubmitAction(action) {
+    if (typeof action !== "function") {
+      this._submitHandler = null;
+    } else {
+      this._submitHandler = action;
+    }
+  }
 
-  _handleSubmit(evt) {
+  // handler interno (ata al submit del form)
+  _onSubmit(evt) {
     evt.preventDefault();
-    if (this._handleSubmit) {
-  this._handleSubmit();
-}
+    if (!this._submitHandler) {
+      return;
+    }
 
+    // Ejecutamos la acción, que debe devolver Promise.
+    // Si devuelve algo que no es Promise, lo envolvemos en Promise.resolve.
+    return Promise.resolve()
+      .then(() => this._submitHandler())
+      .then(() => {
+        // cerrar popup si la acción Promise se resolvió
+        this.close();
+      })
+      .catch((err) => {
+        console.error("Error en acción de confirmación:", err);
+        throw err;
+      });
+  }
+
+  // agregar listeners (incluye listener del form)
   setEventListeners() {
     super.setEventListeners();
     if (this._form) {
-      this._form.addEventListener("submit", this._handleSubmit);
+      this._form.addEventListener("submit", this._onSubmit);
     }
   }
 }
