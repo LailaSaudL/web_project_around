22194607306689e6fffc9a48a10b138ca746af06
LailaSaudL <lailasaud98@gmail.com>
2025-10-12 17:23:30 +0200

    Update PopupWithImage.js

--- scripts/components/PopupWithImage.js
+++ scripts/components/PopupWithImage.js
@@ -3,18 +3,25 @@ import { Popup } from "./Popup.js";
 export class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._popupImage = this._popup.querySelector(".popup__img");
-    this._popupTitle = this._popup.querySelector(".popup__img-title");
+    this._popupImage = this._popup?.querySelector(".popup__img");
+    this._popupTitle = this._popup?.querySelector(".popup__img-title");
   }
 
   open({ name, link }) {
-    if (!this._popupImage || !this._popupTitle) {
-      console.error("Error: elementos de imagen o título no encontrados en el popup");
+    if (!this._popup) {
+      console.error("Popup no encontrado:", this._popup);
       return;
     }
+
+    if (!this._popupImage || !this._popupTitle) {
+      console.error("Elementos .popup__img o .popup__img-title no encontrados dentro del popup.");
+      return;
+    }
+
     this._popupImage.src = link;
     this._popupImage.alt = name;
     this._popupTitle.textContent = name;
+
     super.open();
   }
 }
